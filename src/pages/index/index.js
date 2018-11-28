1f7eb7353c895b2f52f322074ad122ad44025d99
Arthur Bordenyuk <arthur.bordenyuk@gmail.com>
2018-11-28 16:23:58 +0200

    Добавляет на десктопе секцию с ресторанами
    Десктоп, потому что остальное на потом. Сетка используется для будущей
    адаптивности

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,8 @@
 import "./styles/input.pcss";
 import "./styles/address_form.pcss";
 import "./styles/button.pcss";
+import "./styles/restaurants.pcss";
+import "./styles/restaurant.pcss";
 import "./styles/page_header.pcss";
 import "./styles/search_form.pcss";
+import "./styles/page_main.pcss";
